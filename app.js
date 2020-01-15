29be98607d8e56462adef6c67918bd79784e868c
asincole <asincole@gmail.com>
2020-01-15 15:53:28 +0100

    remove needless imports

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var mongoose = require('mongoose');
 var cors = require('cors');
 const dotenv = require('dotenv');
@@ -28,11 +26,7 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 app.use('/cache', cacheRouter);
 
 // catch 404 and forward to error handler
