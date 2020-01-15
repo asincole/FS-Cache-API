e0977f06cdee4e0021ca997ef6593471ef6b4017
asincole <asincole@gmail.com>
2020-01-15 15:52:22 +0100

    remove needless routes and add cors support

--- app.js
+++ app.js
@@ -2,16 +2,28 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var mongoose = require('mongoose');
+var cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var cacheRouter = require('./routes/cache');
+
+
+mongoose.connect(process.env.DATABASE_URI, {useNewUrlParser: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected to the database');
+});
+
+mongoose.set('debug', true);
+
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +31,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+app.use('/cache', cacheRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +50,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app;
